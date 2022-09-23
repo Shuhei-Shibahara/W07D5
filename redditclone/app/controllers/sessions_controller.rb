c50f1efbf96fae1c17a74d08fc772186350cd46c
Shuhei Shibahara <Shoehey06@gmail.com>
2022-09-23 14:10:16 -0700

    finished sessions controller

---
+++ redditclone/app/controllers/sessions_controller.rb
@@ -0,0 +1,26 @@
+class SessionsController < ApplicationController
+    before_action :require_logged_in, only: [:destroy]
+
+    def new
+        @user = User.new
+        render :new
+    end
+
+    def create
+        @user = User.find_by_credentials(params[:user][:username], params[:user][:password])
+        if @user
+            login!(@user)
+            redirect_to users_url
+        else
+            flash.now[:errors] = ["Invalid username or password"]
+            @user = User.find_by_username(params[:user][:username])
+            render :new
+        end
+    end
+
+    def destroy
+        logout!
+        redirect_to new_session_url
+    end
+
+end
